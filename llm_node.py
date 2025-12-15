cc18cf169353baaac0c0376308d3f93673bb050f
riiiiiiin <109677966+riiiiiiin@users.noreply.github.com>
2025-12-15 19:25:32 +0800

    1- llm node maintains context 2- prompts with end_time / location (tests and doc out-of-date)

--- llm_node.py
+++ llm_node.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from service_define.srv import StringForString
+from service_define.srv import SetString
 import threading
 import json
 import os
@@ -21,39 +22,55 @@ class LLMNode(Node):
         self.prompt_check_intent = load_prompt(prompt_dir + '/prompt_check_intent.txt')
         self.prompt_generate_operation = load_prompt(prompt_dir + '/prompt_generate_operation.txt')
             
-        self.srv = self.create_service(StringForString, 'llm/chat', self.handle_llm_request)
-        self.srv_extract_info = self.create_service(StringForString, 'llm/extract_info', self.handle_extract_info)
-        self.srv_check_intent = self.create_service(StringForString, 'llm/check_intent', self.handle_check_intent)
-        self.srv_generate_operation = self.create_service(StringForString, 'llm/generate_operation', self.handle_generate_operation)
-        self.get_logger().info('llm_node ready, service: llm/chat, llm/extract_info, llm/check_intent, llm/generate_operation')
+        self.srv_chat = self.create_service(StringForString, 'llm/chat', self._handle_llm_request)
+        self.srv_extract_info = self.create_service(StringForString, 'llm/extract_info', self._handle_extract_info)
+        self.srv_check_intent = self.create_service(StringForString, 'llm/check_intent', self._handle_check_intent)
+        self.srv_generate_operation = self.create_service(StringForString, 'llm/generate_operation', self._handle_generate_operation)
+        self.srv_clear_history = self.create_service(SetString, 'llm/clear_history', self._handle_clear_history)
+        self.get_logger().info('llm_node ready, service: llm/chat, llm/extract_info, llm/check_intent, llm/generate_operation, llm/clear_history')
         self.client = OpenAI(
             api_key=os.environ.get('DEEPSEEK_API_KEY'),
             base_url="https://api.deepseek.com")
+        
+        self.messages = []
 
-    def handle_llm_request(self, request, response, system_prompt = "You are a helpful assistant."):
+    def _handle_clear_history(self, request, response):
+        self.messages = []
+        response.success = True
+        return response
+    
+    def _append_message(self, message):
+        if len(self.messages) > 10:
+            self.messages = self.messages[2:]
+            self.get_logger().info('Too many messages, poped 2 messages')
+        self.messages.append(message)
+    
+    def _handle_llm_request(self, request, response, system_prompt = "You are a helpful assistant."):
         self.get_logger().info(f'Sending LLM request: "{request.data}"')
+        
+        self._append_message({"role": "user", "content": request.data})
+        
+        messages = [{"role": "system", "content": f"{system_prompt.replace("[CURRENT_TIME]", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))}"},]
+        messages.extend(self.messages)
         
         result = self.client.chat.completions.create(
             model="deepseek-chat",
-            messages=[
-                {"role": "system", "content": f"现在的时间是：{datetime.now()}。下面是你的任务描述：{system_prompt}"},
-                {"role": "user", "content": request.data},
-            ],
+            messages=messages,
             stream=False
         )
         
+        self._append_message(result.choices[0].message)
         self.get_logger().info(f'Received LLM response: "{result.choices[0].message}"')
-
         response.data = result.choices[0].message.content
 
         return response
 
-    def handle_extract_info(self, request, response):
-        return self.handle_llm_request(request, response, self.prompt_extract_info)
-    def handle_check_intent(self, request, response):
-        return self.handle_llm_request(request, response, self.prompt_check_intent)
-    def handle_generate_operation(self, request, response):
-        return self.handle_llm_request(request, response, self.prompt_generate_operation)
+    def _handle_extract_info(self, request, response):
+        return self._handle_llm_request(request, response, self.prompt_extract_info)
+    def _handle_check_intent(self, request, response):
+        return self._handle_llm_request(request, response, self.prompt_check_intent)
+    def _handle_generate_operation(self, request, response):
+        return self._handle_llm_request(request, response, self.prompt_generate_operation)
     
 def main():
     parser = argparse.ArgumentParser()
